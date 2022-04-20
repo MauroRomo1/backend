ca94b77d71a9417f99fa05f7564a19c6f0d16125
Mauro Romo <mauroromo1999@outlook.com>
2022-04-20 19:04:42 -0300

    Corrigiendo bug

--- routes/buscar.js
+++ routes/buscar.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const { buscar } = require("../controllers/buscar");
 
-const router = Router;
+const router = Router();
 
 router.get("/:coleccion/:termino", buscar);
 
